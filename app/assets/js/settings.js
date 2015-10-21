b6911f591d757528cf8d0ef668056479baf83efd
Minsoo Kim <leios76@gmail.com>
2015-10-21 23:08:01 +0900

    현재값 변경

--- app/assets/js/settings.js
+++ app/assets/js/settings.js
@@ -1,4 +1,4 @@
-var ownedList = [2, 3, 5, 11, 16, 17, 18, 21, 24, 25, 26, 28];
+var ownedList = [0, 2, 3, 5, 9, 11, 15, 16, 17, 18, 19, 21, 23, 24, 25, 26, 28];
 var mustHaveList = [3, 5, 11, 16, 17, 18, 21, 24, 25, 26, 28];
 var willHaveList = [9, 15];
-var lastSeed = 5879;
+var lastSeed = 8847;
